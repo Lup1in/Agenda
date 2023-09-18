5defed5f5e5fe6e891a81251b3c20251fd15733b
Lup1in <gabrielcunhaleite@hotmail.com>
2023-09-18 15:44:33 -0300

    Second Commit

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,10 @@
+const HomeModel = require('../models/homemodel');
+HomeModel.create({
+    titulo: 'Titulo Teste 2023',
+    descricao: 'Descrição teste 2023'
+}).then(dados => console.log(dados))
+    .catch(e => console.log(e));
+
 exports.homePage = (req, res) => {
     res.render('index');
 }
